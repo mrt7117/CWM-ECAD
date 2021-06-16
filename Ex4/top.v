b05084edc1f638add2e6e1cb85b0d802d593877e
Cloud User <centos@ip-172-31-7-42.eu-west-2.compute.internal>
2021-06-16 09:37:34 +0000

    Wednesday morning, incomplete

--- Ex4/top.v
+++ Ex4/top.v
@@ -1,7 +1,7 @@
 //////////////////////////////////////////////////////////////////////////////////
 // Exercise #4 - Dynamic LED lights
-// Student Name:
-// Date: 
+// Student Name: Thomas Long
+// Date: 16/06/2021
 //
 //  Description: In this exercise, you need to design a LED based lighting solution, 
 //  following the diagram provided in the exercises documentation. The lights change 
@@ -16,3 +16,50 @@
 //  You need to write the whole file.
 //////////////////////////////////////////////////////////////////////////////////
 
+`timescale 1ns / 100ps
+
+module lights(
+    //define inputs here
+	input clk,
+	input rst,
+	input button,
+	output [2:0] colour
+    );
+
+	reg [2:0] colour;
+
+    //Todo: add user logic
+	always @ (posedge clk or posedge rst) begin
+		if(rst) begin
+		colour = {000}		
+		end
+
+		if (colour == {000}) || (colour ==  {111}) || (colour == {001}) begin #10
+			colour = {001};
+		end
+
+		if(button) begin
+			if (colour = {001} begin #10
+			colour = {010};
+			end
+			else if (colour = {010} begin #10			
+			colour = {011};
+			end
+			else if (colour = {011} begin #10			
+			colour = {100};
+			end
+			else if (colour = {100} begin #10			
+			colour = {101};
+			end
+			else if (colour = {110} begin #10			
+			colour = {011};
+			end
+			else if (colour = {110} begin #10			
+			colour = {001};
+			end
+		end else begin
+		colour = colour;
+		end
+	end
+end module
+	  
--- Ex4/top_tb.v
+++ Ex4/top_tb.v
@@ -1,9 +1,63 @@
 //////////////////////////////////////////////////////////////////////////////////
 // Test bench for Exercise #4 - Dynamic LED lights
-// Student Name:
-// Date: 
+// Student Name: Thomas Long
+// Date: 16/06/2021
 //
 // Description: A testbench module to test Ex4 - Dynamic LED lights
 // You need to write the whole file
 //////////////////////////////////////////////////////////////////////////////////
+`timescale 1ns / 100ps
+
+module top_tb(
+    );
+    
+//Todo: Parameters
+  parameter CLK_PERIOD = 10;
+
+//Todo: Regitsers and wires
+reg clk;
+reg rst;
+reg button;
+wire colour;
+reg err;
+
+    initial
+    begin
+       clk = 1'b0;
+       forever
+         #(CLK_PERIOD/2) clk=~clk;
+     end
+
+initial begin
+	clk=0;
+	rst=0;
+	button=0;
+	err=0;
+	#6
+
+forever begin #6
+	$display("The program runs and it can print");
+	
+end
+end
+
+//Todo: Finish test, check for success
+      initial begin
+        #50 
+        if (err==0)
+          $display("***TEST PASSED! :) ***");
+        $finish;
+      end
+
+//Todo: Instantiate counter module
+light top (
+	.clk (clk),
+	.rst (rst),
+	.button (button),
+	.on_off (on_off),
+	.colour (colour)
+    );
+ 
+endmodule 
+
 
