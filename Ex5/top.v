34cac07d934f32719c7c2989a708f66cf5e36afe
Cloud User <centos@ip-172-31-7-42.eu-west-2.compute.internal>
2021-06-16 13:45:05 +0000

    A complete solution to Ex5

--- Ex5/top.v
+++ Ex5/top.v
@@ -1,7 +1,7 @@
 //////////////////////////////////////////////////////////////////////////////////
 // Exercise #5 - Air Conditioning
-// Student Name:
-// Date: 
+// Student Name: Thomas Long
+// Date: 16/06/2021
 //
 //  Description: In this exercise, you need to an air conditioning control system
 //  According to the state diagram provided in the exercise.
@@ -12,4 +12,40 @@
 //  outputs:
 //           heating, cooling
 //////////////////////////////////////////////////////////////////////////////////
+// 18 = 10010
+// 20 = 10100
+// 22 = 10110
+
+`timescale 1ns / 100ps
+
+module AC(
+    input clk,
+    input [4:0] temperature,
+    output heating,
+    output cooling
+    );
+
+reg [1:0] state;
+wire heating, cooling;
+
+always @ (posedge clk) begin
+	if (state == 2'b00) begin //idle
+		state <= (temperature <= 5'b10010) ? 2'b10: 2'b00;
+		state <= (temperature >= 5'b10110) ? 2'b01: 2'b00;
+	end
+
+  else if (state== 2'b01) begin		// cooling
+       state <= (temperature <= 5'b10100) ? 2'b00: 2'b01;
+      end
+
+  else if (state== 2'b10) begin	// heating state
+       state <= (temperature >= 5'b10100) ? 2'b00: 2'b10;	//turn off heating if T>= 20
+      end
+
+  else begin   			// forbidden state
+      state= 2'b00;
+     end
+end
+
+endmodule
 
--- Ex5/top_tb.v
+++ Ex5/top_tb.v
@@ -1,9 +1,79 @@
 //////////////////////////////////////////////////////////////////////////////////
 // Test bench for Exercise #5 - Air Conditioning
-// Student Name:
-// Date: 
+// Student Name: Thomas Long
+// Date: 16/06/2021
 //
 // Description: A testbench module to test Ex5 - Air Conditioning
 // You need to write the whole file
 //////////////////////////////////////////////////////////////////////////////////
+
+`timescale 1ns / 100ps
+
+module top_tb(
+    );
+    
+  parameter CLK_PERIOD = 10;
+  
+  reg clk;
+  reg err;
+  reg [4:0] temperature; 
+  wire heating;
+  wire cooling;
+
+initial begin 
+    clk= 1'b0;
+    forever begin
+      # (CLK_PERIOD/2) clk=~clk;
+end
+end
+
+ initial begin
+    err = 0;
+    temperature = 5'b11001; // 25		5'b01111 would be 15
+    
+    forever begin
+    	#CLK_PERIOD
+	if((temperature<=5'b10010)&(heating != 1'b1)&(cooling != 1'b0)) begin 
+        	err = 1;
+		$display("Heating ERROR: Incorrect AC function below 18deg");
+    	end
+	if ((temperature>=5'b10110)&(heating != 1'b0)&(cooling != 1'b1)) begin
+        	err = 1;
+		$display("Cooling ERROR: Incorrect AC function above 22deg");
+    	end 
+	if ((temperature>5'b10010)&(temperature<5'b10110)&(heating != 1'b0)&(cooling != 1'b0)) begin
+        	err = 1;
+		$display("Idle ERROR: Incorrect AC function between 18deg and 22deg");
+    	end 
+	    
+    	temperature <= temperature + 5'b00001;
+    	if (temperature>5'b11111)
+		temperature<=5'b00000;
+	    
+    	end 
+  end
+  
+    initial begin
+    #500
+    if (err==0)
+        $display("***TEST PASSED! :) ***");
+       $finish;
+  end
+  
+    AC top(
+    .clk (clk),
+    .temperature (temperature),
+    .heating (heating),
+    .cooling (cooling)
+  );
+  
+endmodule
+
+
+
+
+
+
+
+
 
