88e89886352b178bcd71eb514e5dec4b10bb67ed
Cloud User <centos@ip-172-31-7-42.eu-west-2.compute.internal>
2021-06-17 11:39:27 +0000

    A complete solution to Ex1

--- Ex6/top.v
+++ Ex6/top.v
@@ -1,7 +1,7 @@
 //////////////////////////////////////////////////////////////////////////////////
 // Exercise #6 - RGB Colour Converter
-// Student Name:
-// Date: 
+// Student Name: Thomas Long
+// Date: 17/06/2021
 //
 //
 //  Description: In this exercise, you need to design a memory with 8 entries, 
@@ -15,3 +15,23 @@
 //
 //////////////////////////////////////////////////////////////////////////////////
 
+`timescale 1ns / 100ps
+
+module RGB_convertor (
+	input clk,
+	input enable,
+	input [2:0] colour,
+	output [23:0] rgb
+	);
+
+blk_mem_gen_2 colour_converter (
+  .clka (clk),    // input wire clka
+  .ena (enable),      // input wire ena
+  .wea (1'b0),      // input wire [0 : 0] wea
+  .addra (colour),  // input wire [2 : 0] addra
+  .dina (24'h0),    // input wire [23 : 0] dina
+  .douta (rgb)  // output wire [23 : 0] douta
+);
+
+
+endmodule
